dc347b62b78d7819e2af3e44ea6e7a29caf71cfd
flyingwhitewhale <30478118+flyingwhitewhale@users.noreply.github.com>
2021-09-30 21:57:19 +0800

    Update geo_location.js

--- Scripts/geo_location.js
+++ Scripts/geo_location.js
@@ -18,7 +18,7 @@
  
   var message = ""
   const paras = ["ip","organization","isp","asn","asn_organization","country","region","city"]
-  const paran = ["IP","机构","ISP","AS号","AS名称","国家","地区","城市"]
+  const paran = ["IP","Owner","ISP","ASN","AS","Country","Region","City"]
   $task.fetch(myRequest).then(response => {
     message = response? json2info(response.body,paras) : ""
       $done({"title":"🔎 IP.SB 查询结果","message":message});
